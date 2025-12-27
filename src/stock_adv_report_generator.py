69889286d7c69a0d0ff8a4fceb8e3a37aeb10ef6
deghislain <deghislain@gmail.com>
2025-12-26 21:44:29 -0400

    Code clean up

--- src/stock_adv_report_generator.py
+++ src/stock_adv_report_generator.py
@@ -6,7 +6,6 @@
 import asyncio, logging
 
 from stock_adv_analysis_engine import FinAnalystAgent
-from stock_adv_reviewer_agent import QualityCheckAgent
 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
@@ -14,7 +13,6 @@ logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %
 class ReportGeneratorAgent:
     def __init__(self):
         self.fin_analyst_agent = FinAnalystAgent()
-        self.quality_check_agent = QualityCheckAgent()
         self.fund_analysis = None
         self.reviewed_fund_analysis = None
 
@@ -27,22 +25,9 @@ class ReportGeneratorAgent:
             f"******************************_perform_fundamental_analysis END with output: {self.fund_analysis}")
         return self.fund_analysis
 
-    async def _review_and_improve_fundamental_analysis(self):
-        logging.info(f"******************************_review_and_improve_fundamental_analysis START "
-                     f"with input: *************{self.fund_analysis}")
-        self.reviewed_fund_analysis = await self.quality_check_agent.review_and_improve_fundamental_analysis(
-            self.fund_analysis)
-        logging.info(
-            f"******************************_review_and_improve_fundamental_analysis END with output: {self.reviewed_fund_analysis}")
-        return self.reviewed_fund_analysis
-
     async def generate_report(self, stock_symbol: str):
         logging.info(f"******************************generate_report START with input: {stock_symbol}")
-        '''
-        self.fundamental_data = await self._fetch_fundamental_data(stock_symbol)
-        if self.fundamental_data:
-            self.fund_analysis = await self._perform_fundamental_analysis()
-        '''
+
         if stock_symbol:
             self.fund_analysis = await self._perform_fundamental_analysis(stock_symbol)
 
